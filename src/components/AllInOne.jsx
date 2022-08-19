5ef8abde045934bf284eb12ce899588c0fda0e32
George <jacemilrp@gmail.com>
2022-08-19 18:32:24 +0100

    initial commit

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,151 @@
+import { CheckIcon } from "@heroicons/react/outline";
+import React from "react";
+
+function AllInOne() {
+	return (
+		<div name="platforms" className="w-full my-32">
+			<div className="max-w-[1240px] mx-auto px-2">
+				<h2 className="text-5xl font-bold text-center">
+					All-In-One Platform
+				</h2>
+				<p className="text-2xl py-8 text-gray-500 text-center">
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+					Repellat dolor eum delectus aperiam facere dolorem
+					distinctio iure aspernatur, ipsa sequi?
+				</p>
+
+				<div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+					<div className="flex border-2 border-indigo-600 rounded-xl p-4">
+						<div>
+							<CheckIcon className="w-10 mr-4 text-green-600" />
+						</div>
+						<div>
+							<h3 className="font-bold text-2xl">
+								Notifications
+							</h3>
+							<p className="text-xl pt-2 pb-4">
+								Lorem ipsum dolor, sit amet consectetur
+								adipisicing elit. Illum nam aliquid, sunt
+								dolorem sapiente assumenda.
+							</p>
+						</div>
+					</div>
+
+					<div className="flex border-2 border-indigo-600 rounded-xl p-4">
+						<div>
+							<CheckIcon className="w-10 mr-4 text-green-600" />
+						</div>
+						<div>
+							<h3 className="font-bold text-2xl">
+								Notifications
+							</h3>
+							<p className="text-xl pt-2 pb-4">
+								Lorem ipsum dolor, sit amet consectetur
+								adipisicing elit. Illum nam aliquid, sunt
+								dolorem sapiente assumenda.
+							</p>
+						</div>
+					</div>
+
+					<div className="flex border-2 border-indigo-600 rounded-xl p-4">
+						<div>
+							<CheckIcon className="w-10 mr-4 text-green-600" />
+						</div>
+						<div>
+							<h3 className="font-bold text-2xl">
+								Notifications
+							</h3>
+							<p className="text-xl pt-2 pb-4">
+								Lorem ipsum dolor, sit amet consectetur
+								adipisicing elit. Illum nam aliquid, sunt
+								dolorem sapiente assumenda.
+							</p>
+						</div>
+					</div>
+
+					<div className="flex border-2 border-indigo-600 rounded-xl p-4">
+						<div>
+							<CheckIcon className="w-10 mr-4 text-green-600" />
+						</div>
+						<div>
+							<h3 className="font-bold text-2xl">
+								Notifications
+							</h3>
+							<p className="text-xl pt-2 pb-4">
+								Lorem ipsum dolor, sit amet consectetur
+								adipisicing elit. Illum nam aliquid, sunt
+								dolorem sapiente assumenda.
+							</p>
+						</div>
+					</div>
+
+					<div className="flex border-2 border-indigo-600 rounded-xl p-4">
+						<div>
+							<CheckIcon className="w-10 mr-4 text-green-600" />
+						</div>
+						<div>
+							<h3 className="font-bold text-2xl">
+								Notifications
+							</h3>
+							<p className="text-xl pt-2 pb-4">
+								Lorem ipsum dolor, sit amet consectetur
+								adipisicing elit. Illum nam aliquid, sunt
+								dolorem sapiente assumenda.
+							</p>
+						</div>
+					</div>
+
+					<div className="flex border-2 border-indigo-600 rounded-xl p-4">
+						<div>
+							<CheckIcon className="w-10 mr-4 text-green-600" />
+						</div>
+						<div>
+							<h3 className="font-bold text-2xl">
+								Notifications
+							</h3>
+							<p className="text-xl pt-2 pb-4">
+								Lorem ipsum dolor, sit amet consectetur
+								adipisicing elit. Illum nam aliquid, sunt
+								dolorem sapiente assumenda.
+							</p>
+						</div>
+					</div>
+
+					<div className="flex border-2 border-indigo-600 rounded-xl p-4">
+						<div>
+							<CheckIcon className="w-10 mr-4 text-green-600" />
+						</div>
+						<div>
+							<h3 className="font-bold text-2xl">
+								Notifications
+							</h3>
+							<p className="text-xl pt-2 pb-4">
+								Lorem ipsum dolor, sit amet consectetur
+								adipisicing elit. Illum nam aliquid, sunt
+								dolorem sapiente assumenda.
+							</p>
+						</div>
+					</div>
+
+					<div className="flex border-2 border-indigo-600 rounded-xl p-4">
+						<div>
+							<CheckIcon className="w-10 mr-4 text-green-600" />
+						</div>
+						<div>
+							<h3 className="font-bold text-2xl">
+								Notifications
+							</h3>
+							<p className="text-xl pt-2 pb-4">
+								Lorem ipsum dolor, sit amet consectetur
+								adipisicing elit. Illum nam aliquid, sunt
+								dolorem sapiente assumenda.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default AllInOne;
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { CheckIcon, XIcon } from "@heroicons/react/outline";
+
+function Pricing() {
+	return (
+		<div name="pricing" className="w-full text-white my-24">
+			<div className="w-full h-[800px] bg-slate-900 absolute mix-blend-overlay"></div>
+
+			<div className="max-w-[1240px] mx-auto py-12">
+				<div className="text-center py-8 text-slate-300">
+					<h2 className="text-3xl uppercase">Pricing</h2>
+					<h3 className="text-5xl font-bold text-white py-8">
+						The right price for your needs.
+					</h3>
+					<p className="text-3xl">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Quis voluptas at praesentium voluptate obcaecati
+						asperiores, dolore nostrum nisi veritatis ipsam?
+					</p>
+				</div>
+
+				<div className="grid md:grid-cols-2 ">
+					<div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+						<span className="uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-xl text-sm font-bold">
+							Standard
+						</span>
+						<div>
+							<p className="text-6xl font-bold py-4 flex">
+								£49
+								<span className="text-xl text-slate-500 flex flex-col justify-end">
+									/mo
+								</span>
+							</p>
+						</div>
+						<p className="text-2xl py-8 text-slate-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing.
+						</p>
+						<div>
+							<p className="flex py-3 text-2xl relative">
+								<CheckIcon className="w-8 mr-5 text-green-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<p className="flex py-3 text-2xl relative">
+								<XIcon className="w-8 mr-5 text-red-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<p className="flex py-3 text-2xl relative">
+								<CheckIcon className="w-8 mr-5 text-green-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<p className="flex py-3 text-2xl relative">
+								<XIcon className="w-8 mr-5 text-red-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<p className="flex py-3 text-2xl relative">
+								<XIcon className="w-8 mr-5 text-red-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<button className="w-full py-4 my-4 text-xl bg-slate-800 border-slate-800">
+								Get Started
+							</button>
+						</div>
+					</div>
+
+					<div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+						<span className="uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-xl text-sm font-bold">
+							Pro
+						</span>
+						<div>
+							<p className="text-6xl font-bold py-4 flex">
+								£99
+								<span className="text-xl text-slate-500 flex flex-col justify-end">
+									/mo
+								</span>
+							</p>
+						</div>
+						<p className="text-2xl py-8 text-slate-500">
+							Lorem ipsum dolor sit amet, consectetur adipisicing.
+						</p>
+						<div>
+							<p className="flex py-3 text-2xl relative">
+								<CheckIcon className="w-8 mr-5 text-green-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<p className="flex py-3 text-2xl relative">
+								<CheckIcon className="w-8 mr-5 text-green-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<p className="flex py-3 text-2xl relative">
+								<CheckIcon className="w-8 mr-5 text-green-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<p className="flex py-3 text-2xl relative">
+								<CheckIcon className="w-8 mr-5 text-green-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<p className="flex py-3 text-2xl relative">
+								<CheckIcon className="w-8 mr-5 text-green-600" />
+								Lorem, ipsum dolor.
+							</p>
+							<button className="w-full py-4 my-4 text-xl bg-slate-800 border-slate-800">
+								Get Started
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Pricing;
